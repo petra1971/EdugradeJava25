97f355f3bc84db152cc1d492a74a8121eb47aa3f
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 23:58:50 +0200

    Moved main again to the same package as all other classes

--- 1_oop/oop-assignment/src/main/java/org/game/Main.java
+++ 1_oop/oop-assignment/src/main/java/org/game/Main.java
@@ -1,6 +1,4 @@
-package org.gameEngine;
-
-import org.characters.Weapon;
+package org.game;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
