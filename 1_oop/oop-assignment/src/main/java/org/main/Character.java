a28eeb066307540a7d8c4ef81e442d6259ae9244
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 21:28:31 +0200

    Add first version of abstract Character class

---
+++ 1_oop/oop-assignment/src/main/java/org/main/Character.java
@@ -0,0 +1,23 @@
+package org.main;
+
+public abstract class Character {
+    protected String name;
+    protected int maxHealth;
+    protected int health;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+    }
+
+    abstract boolean isAlive(int health);
+    abstract void takeDamage(int damage);
+    abstract void attack(int strength);
+
+    void status() {
+        //TODO
+        System.out.println();
+    }
+
+
+}
