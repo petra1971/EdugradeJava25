dedd71caf6617fb48a19fa025f11ab9f082cc258
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-02 00:07:55 +0200

    Added gainWeapon() method and more logic to attack()

--- 1_oop/oop-assignment/src/main/java/org/game/Hero.java
+++ 1_oop/oop-assignment/src/main/java/org/game/Hero.java
@@ -1,19 +1,25 @@
 package org.game;
 
 public class Hero extends Character {
-    private int power = 10;
+    private int basicPower = 10;
+    private Weapon weapon;
+    private int totalPower;
     private int xp = 0;
     private int level = 1;
-    private Weapon weapon;
 
     public Hero(String name) {
         super(name);
     }
 
+    public void gainWeapon(Weapon newWeapon) {
+        weapon = newWeapon;
+    }
+
     void attack(int value) {
-        power += value;
+        totalPower = basicPower + weapon.getPower();
+        System.out.println("Hero attacks with " + weapon + " and total power of " + totalPower);
         //En styrka sätts som jämförs med Monstrets styrka. Högst styrka vinner. Vapen ger extra styrka
         // Den som förlorar tar damage. När health är 0 för den ena är rundan slut. Hero får mer xp (gainXp())
-        //och även eventuellt guld. Hälsan återställs till max.
-}
+        //och även eventuellt guld. Hälsan återställs till max.}
+    }
 }
