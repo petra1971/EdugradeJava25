7b9e8117c496084624bece2670e0222451b08751
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 21:34:51 +0200

    Implement Weapon

--- 1_oop/oop-assignment/src/main/java/org/main/Weapon.java
+++ 1_oop/oop-assignment/src/main/java/org/main/Weapon.java
@@ -1,4 +1,16 @@
 package org.main;
 
 public class Weapon {
+
+    private String name;
+    private int power;
+
+    public Weapon(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public void printInfo() {
+        System.out.println("Strength name: " + name + "and Injury: " + power);
+    }
 }
