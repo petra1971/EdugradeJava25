3459594588ffa6fb4dd5c7d55bd16ee362683ecc
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 23:49:04 +0200

    Added power to Hero and moved class to characters package

---
+++ 1_oop/oop-assignment/src/main/java/org/characters/Hero.java
@@ -0,0 +1,20 @@
+package org.characters;
+
+public class Hero extends Character {
+    private int power = 10;
+    private int xp = 0;
+    private int level = 1;
+    private Weapon weapon;
+
+    public Hero(String name) {
+        super(name);
+    }
+
+
+
+    void attack(int power) {
+        //En styrka sätts som jämförs med Monstrets styrka. Högst styrka vinner. Vapen ger extra styrka
+        // Den som förlorar tar damage. När health är 0 för den ena är rundan slut. Hero får mer xp (gainXp())
+        //och även eventuellt guld. Hälsan återställs till max.
+}
+}
