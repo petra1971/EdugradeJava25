dd4347a2dcec57e0c666a739dc623c90691dee57
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 23:49:46 +0200

    Import weapon class to be able to test

--- 1_oop/oop-assignment/src/main/java/org/gameEngine/Main.java
+++ 1_oop/oop-assignment/src/main/java/org/gameEngine/Main.java
@@ -1,6 +1,6 @@
 package org.gameEngine;
 
-import org.entities.Weapon;
+import org.characters.Weapon;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
