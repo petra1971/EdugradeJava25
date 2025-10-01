15e596675ec7bf4eb81b86c8e8396e50f28f23ca
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-02 00:53:56 +0200

    Implemented a message in attack() and added status() method

--- 1_oop/oop-assignment/src/main/java/org/game/Hero.java
+++ 1_oop/oop-assignment/src/main/java/org/game/Hero.java
@@ -15,10 +15,17 @@ public class Hero extends Character {
     }
 
     int attack(int value) {
-        System.out.println("Hero attacks with " + weapon + " and total power of " + totalPower);
-        return basicDamage + weapon.getDamage();
+        int totalDamage = basicDamage + weapon.getDamage();
+        System.out.println("Hero attacks with " + weapon + " and total power of " + totalDamage);
+
+        return totalDamage;
         //En damage sätts som jämförs med Monstrets damage. Högst damage (skada man åsamkar den andra) vinner. Vapen ger extra damage
         // Den som förlorar tar damage. När health är 0 för den ena är rundan slut. Hero får mer xp (gainXp())
         //och även eventuellt guld. Hälsan återställs till max.}
+    }
+
+    @Override
+    void status() {
+        System.out.println("Name is " + name + ", basic damage " + basicDamage + ", weapon " + weapon + ", health " + health + "/" + maxHealth);
     }
 }
