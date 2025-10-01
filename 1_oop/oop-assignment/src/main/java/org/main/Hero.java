26c930dafed5a25ddd5d044958573d50a45b0b79
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-01 22:45:56 +0200

    Added Hero concrete class, first version

---
+++ 1_oop/oop-assignment/src/main/java/org/main/Hero.java
@@ -0,0 +1,15 @@
+package org.main;
+
+public class Hero extends Character{
+    private int xp = 0;
+    private int level = 1;
+    private Weapon weapon;
+
+    public Hero(String name) {
+        super(name);
+    }
+
+    public void attack(int power) {
+        //TODO
+    }
+}
