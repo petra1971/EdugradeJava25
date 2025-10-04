90de541e86024701e564908b3629c6ddb8ad474a
Petra Andreasson <petra.andreasson.java25@edu.edugrade.se>
2025-10-04 21:08:42 +0200

    Feat: replace Place with SafePlace

---
+++ 1_oop/oop-assignment/src/main/java/org/game/entities/SafePlace.java
@@ -0,0 +1,13 @@
+package org.game.entities;
+
+public class SafePlace {
+    private String name;
+    private int carrotReward;
+    private int xpReward;
+
+    public SafePlace(String name, int carrotReward, int xpReward) {
+        this.name = name;
+        this.carrotReward = carrotReward;
+        this.xpReward = xpReward;
+    }
+}
